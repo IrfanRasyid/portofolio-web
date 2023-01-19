4ee74214270b90815ae5e6c4826d8ce02f81ac4f
IrfanRasyid <41520010081@student.mercubuana.ac.id>
2023-01-19 14:28:48 +0700

    Portfol

---
+++ tailwind-project/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#312e81',
+        seconary: '#6b7280',
+        dark: '#0f172a',
+    },
+    screens: {
+      '2xl': '1320px',
+  },
+},
+  plugins: [],
+},
+};
